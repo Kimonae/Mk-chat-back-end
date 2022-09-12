33b95ef6f22a6577b31c3883aab2ccb0dfbc9d9f
Kimonae <shinokasu@gmail.com>
2022-09-12 11:21:33 +0200

    update with chatRoom

---
+++ projetSlack/src/main/java/com/m2i/projetSlack/controller/ChatRoomController.java
@@ -0,0 +1,11 @@
+package com.m2i.projetSlack.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/Room")
+
+public class ChatRoomController {
+
+}
---
+++ projetSlack/src/main/java/com/m2i/projetSlack/repository/ChatRoomRepository.java
@@ -0,0 +1,18 @@
+package com.m2i.projetSlack.repository;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import com.m2i.projetSlack.entity.ChatRoom;
+
+
+
+public interface ChatRoomRepository extends  JpaRepository<ChatRoom, Integer> {
+	
+	@Query("SELECT cr FROM ChatRoom cr WHERE cr.chatroom IN :chatr")
+	public List<ChatRoom> getRoomInListChatRoom(@Param("chatr") List<String> RoomName);
+
+}
