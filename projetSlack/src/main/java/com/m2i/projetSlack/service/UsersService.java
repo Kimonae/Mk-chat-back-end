e1307ab25fd918e6328831bc188311cbcb0a9d3e
Maryline-Bellanger <maryline.bellanger45@gmail.com>
2022-09-12 12:00:57 +0200

    updated files on all folders users

--- projetSlack/src/main/java/com/m2i/projetSlack/service/UsersService.java
+++ projetSlack/src/main/java/com/m2i/projetSlack/service/UsersService.java
@@ -1,5 +1,46 @@
 package com.m2i.projetSlack.service;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.m2i.projetSlack.entity.Users;
+import com.m2i.projetSlack.repository.UsersRepository;
+
+@Service
 public class UsersService {
+	
+	@Autowired
+	UsersRepository repo;
+	
+	public List<Users> getUser(){
+		return repo.findAll();
+	}
+	
+	public Users postUser(Users user) {
+		return repo.save(user);
+	}
+	
+	public Users putUser(Users user) {
+		Users newUsers = repo.findById(user.getId()).orElse(null);
+		
+		newUsers.setLastname(user.getLastname());
+		newUsers.setFirstname(user.getFirstname());
+		
+		repo.save(newUsers);
+		
+		return newUsers;
+	}
+	
+	public String deleteUser(int id) {
+		if (repo.findById(id).isPresent())
+		{
+			repo.deleteById(id);
+			return "User deleted : " +id;
+		}
+		
+		return "User not found : " + id;
+	}
 
 }
