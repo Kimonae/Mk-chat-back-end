4dab1d5727bc778abcafc6c0a2e09c8412e17e11
Kimonae <shinokasu@gmail.com>
2022-09-13 09:37:18 +0200

    change generated value

--- projetSlack/src/main/java/com/m2i/projetSlack/entity/TchatRoom.java
+++ projetSlack/src/main/java/com/m2i/projetSlack/entity/TchatRoom.java
@@ -4,6 +4,7 @@ import java.util.Date;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
 
@@ -13,7 +14,7 @@ import javax.persistence.Table;
 public class TchatRoom {
 	
 	@Id
-	@GeneratedValue
+	@GeneratedValue(strategy= GenerationType.IDENTITY)
 	private int id;
 	private String name;
 	private Date CreationDate;
