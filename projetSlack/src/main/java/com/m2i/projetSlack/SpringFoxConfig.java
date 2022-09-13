e1f2c4afa2d6207e849b7865848aed9a941745b5
Kimonae <shinokasu@gmail.com>
2022-09-13 16:48:46 +0200

    add query

--- projetSlack/src/main/java/com/m2i/projetSlack/SpringFoxConfig.java
+++ projetSlack/src/main/java/com/m2i/projetSlack/SpringFoxConfig.java
@@ -10,8 +10,6 @@ import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 
-@SpringBootApplication
-@EnableWebMvc
 @Configuration
 public class SpringFoxConfig {                                    
     @Bean
