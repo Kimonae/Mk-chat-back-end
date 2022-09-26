72a8ca68cba8dfff731739f91f29ab017e4bdc40
macrod-dev <gmikangamani@gmail.com>
2022-09-26 22:13:48 +0200

    ajout de l'entité photos

--- projetSlack/src/main/java/com/m2i/projetSlack/entity/Photos.java
+++ projetSlack/src/main/java/com/m2i/projetSlack/entity/Photos.java
@@ -1,5 +1,51 @@
 package com.m2i.projetSlack.entity;
 
-public class Photos {
 
+@Entity
+@Table(name= "photos")
+public class Photos {
+	
+	@Id
+	@Generatedvalue(strategy= GeneratedType.IDENTITY)
+	private int id;
+	private String nom;
+	private String extension;
+	
+	public Photos() {
+		super();
+		
+	}
+
+	public Photos(int id, String nom, String extension) {
+		super();
+		this.id = id;
+		this.nom = nom;
+		this.extension = extension;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getNom() {
+		return nom;
+	}
+
+	public void setNom(String nom) {
+		this.nom = nom;
+	}
+
+	public String getExtension() {
+		return extension;
+	}
+
+	public void setExtension(String extension) {
+		this.extension = extension;
+	}
+	
+	
 }
