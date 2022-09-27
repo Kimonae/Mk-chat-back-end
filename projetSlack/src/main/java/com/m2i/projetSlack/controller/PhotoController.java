993ba81826ce7005c4efc99335ebbaf84a2bfb1b
macrod-dev <gmikangamani@gmail.com>
2022-09-27 17:35:20 +0200

    import classe PhotoController

--- projetSlack/src/main/java/com/m2i/projetSlack/controller/PhotoController.java
+++ projetSlack/src/main/java/com/m2i/projetSlack/controller/PhotoController.java
@@ -1,6 +1,19 @@
 package com.m2i.projetSlack.controller;
 
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
 import com.m2i.projetSlack.entity.Photo;
+import com.m2i.projetSlack.service.PhotoService;
 
 @RestController
 @RequestMapping("/api/photo")
