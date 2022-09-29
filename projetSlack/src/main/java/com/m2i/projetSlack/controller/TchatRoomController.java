485bf0db8b0570956a19e37411d1fc65cf54ab47
Kimonae <70099141+Kimonae@users.noreply.github.com>
2022-09-29 11:02:58 +0200

    cross origin

--- projetSlack/src/main/java/com/m2i/projetSlack/controller/TchatRoomController.java
+++ projetSlack/src/main/java/com/m2i/projetSlack/controller/TchatRoomController.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -17,6 +18,7 @@ import com.m2i.projetSlack.service.TchatRoomService;
 
 
 @RestController
+@CrossOrigin(origins ="http:/:localhost:4200")
 @RequestMapping("/api/tchatroom")
 
 public class TchatRoomController {
